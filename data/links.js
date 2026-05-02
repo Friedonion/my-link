e863b45701a50a0004076a7a2f3be075d7da5bc9
Friedonion <rkdwnd333@naver.com>
2026-05-02 17:28:05 +0900

    feat: 링크 더미 데이터(dummy data) 추가

---
+++ data/links.js
@@ -0,0 +1,32 @@
+export const dummyLinks = [
+  {
+    id: "1",
+    title: "Instagram",
+    url: "https://instagram.com/my_profile",
+    icon: "instagram" // 선택 속성
+  },
+  {
+    id: "2",
+    title: "YouTube",
+    url: "https://youtube.com/@my_channel",
+    icon: "youtube"
+  },
+  {
+    id: "3",
+    title: "Naver Blog",
+    url: "https://blog.naver.com/my_blog"
+    // icon 생략
+  },
+  {
+    id: "4",
+    title: "GitHub",
+    url: "https://github.com/my_username",
+    icon: "github"
+  },
+  {
+    id: "5",
+    title: "Portfolio",
+    url: "https://my-portfolio.com",
+    icon: "briefcase"
+  }
+];
